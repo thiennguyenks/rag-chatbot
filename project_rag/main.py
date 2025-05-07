cd25761195fab3aa56c2ac000f3a67b4f36bdb2d
thiennguyenks <maithien07022004@gmail.com>
2025-05-07 18:37:39 +0700

    Update main.py

--- project_rag/main.py
+++ project_rag/main.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
-import os
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+import os
 
 app = Flask(__name__)
 
-# Load và index tài liệu từ thư mục "data"
+# Load dữ liệu từ thư mục 'data'
 documents = SimpleDirectoryReader("data").load_data()
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
