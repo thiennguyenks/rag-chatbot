cc950cb2af7ad49a3125868ee849a86a5dca6546
thiennguyenks <maithien07022004@gmail.com>
2025-05-07 18:27:59 +0700

    Update main.py

--- project_rag/main.py
+++ project_rag/main.py
@@ -18,6 +18,8 @@ def ask():
     response = query_engine.query(query)
     return jsonify({"response": str(response)})
 
-if __name__ == "__main__":
+import os
+
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
