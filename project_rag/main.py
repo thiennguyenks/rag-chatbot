3260767fc587efe9c9811f68d78f0226c56039b0
thiennguyenks <maithien07022004@gmail.com>
2025-05-07 16:46:56 +0700

    Add files via upload

---
+++ project_rag/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+
+app = Flask(__name__)
+
+# Load và index tài liệu trong thư mục data
+documents = SimpleDirectoryReader('data').load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    data = request.get_json()
+    query = data.get('query', '')
+    if not query:
+        return jsonify({'error': 'Missing query'}), 400
+    response = query_engine.query(query)
+    return jsonify({'response': str(response)})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7860)
